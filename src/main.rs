291e6c18d649b30e174ad079379a5989843a9aab
Andarii <AndariiDev@proton.me>
2025-10-16 01:06:18 +0200

    Initial commit: Add core logic (dependency checking)
    Implemented core functionality of the dependency detective tool to allow it to check a list of dependencies from a file, iterating through them and checking for their existence in the file system.
    
    It uses PathBuf to safely join paths and provides clear, user-friendly output (thanks to {:#?}) for both existing and missing dependencies.

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::path::Path;
+use std::fs;
+
+fn main() {
+    let project_root = Path::new("../my_c_project");
+    let test_repo_path = project_root.join("../my_c_project/dependencies.txt");
+    
+    if test_repo_path.exists() {
+        println!("This file exists!");
+        
+        let content = fs::read_to_string(test_repo_path).unwrap();
+        
+        let lines: Vec<&str> = content.lines().collect();
+
+        println!("\nFound the following dependencies to check:\n{:#?}", lines);
+
+        for line in lines {
+            let full_path = project_root.join(line);
+
+            if full_path.exists() {
+                println!("Exists: {:#?}", full_path);
+                
+            } else {
+
+                println!("Mssing (ENOENT)");
+            }
+            
+        }
+    
+    } else {
+        println!("This file doesn't exist!");
+    }
+}
