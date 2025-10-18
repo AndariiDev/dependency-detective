edf1d0e30ab589453d66d778edf100f18bf21830
Andarii <AndariiDev@proton.me>
2025-10-18 20:43:37 +0200

    feat: add unit test (first ever!)

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let project_root = Path::new("../my_c_project");
-    let test_repo_path = project_root.join("../my_c_project/dependencies.txt");
+    let test_repo_path = project_root.join("dependencies.txt");
     
     if !test_repo_path.exists() {
         println!("This file doesn't exist!");
@@ -34,4 +34,34 @@ fn main() -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
+}
+
+
+#[cfg(test)] // first ever attempt at writing a unit test
+
+mod tests {
+    
+    use super::*; // bring everything from main into scope
+    use std::path::Path;
+
+    #[test]
+    
+    fn success() {
+        
+        // ARRANGE: Set up smallest possible environment; only need root and a dep here 
+        let project_root = Path::new("dummy_root");
+        let dependency_name = "missing_file.h";
+
+        // ACT: now execute logic to test (path joining)
+        let full_path = project_root.join(dependency_name);
+        
+        // ASSERT: Check expected outcome
+        // 1. check PathBuf was constructed correctly (optional, but for learning purposes)
+        assert_eq!(full_path.to_string_lossy(), "dummy_root/missing_file.h");
+
+        // 2. crucially, we haven't created the file, so it must not exist
+        assert!(!full_path.exists(), "Test failed: Path should not exist");
+        
+    }
+    
 }
